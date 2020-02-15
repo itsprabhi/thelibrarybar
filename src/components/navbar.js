87a9b3fec2a758925882f6c686899ca67681241b
itsprabhi <iamprabhie@gmail.com>
2020-02-15 18:54:01 -0500

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,61 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
-    return (
-        <div>
-            <div className = 'navbar'>
-                <div className = 'container'>
-                    <div className = 'nav-container'>
-                        <div className = 'navbar-brand'>
-                            <div className = 'navbar-brand-text'>
-                                <h3>
-                                    The Library Bar
-                                </h3>
+
+
+export class Navbar extends Component {
+    state = {
+        onOff:false
+    }
+    toggleSwitch = () => {
+        if(this.state.onOff){
+            this.setState({
+                onOff:false
+            })
+        }else{
+            this.setState({
+                onOff:true
+            })
+        }
+    }
+    render() {
+        let navOff
+        let navOn = 'display-nav'
+        let navClass = this.state.onOff ? navOff : navOn
+        return (
+            <div>
+                 <div className = 'navbar'>
+                    <div className = 'container'>
+                        <div className = 'nav-container'>
+                            <div className = 'navbar-brand'>
+                                <div className = 'navbar-brand-text'>
+                                    <h3>
+                                        The Library Bar
+                                    </h3>
+                                </div>
                             </div>
-                        </div>
-                        <div className = 'navbar-list'>
-                            <div className = 'navbar-items'>
-                                <ul>
-                                    <li className = 'navbar-item'>
-                                        <Link to = '/'>
-                                            Home
-                                        </Link>
-                                    </li>
-                                    <li className = 'navbar-item'>
-                                        <Link to = '/menu'>
-                                            Menu
-                                        </Link>
-                                    </li>
-                                    <li className = 'navbar-item'>
-                                        <Link to = '/contact'>
-                                            Contact
-                                        </Link>
-                                    </li>
-                                    <li className = 'navbar-item'>
-                                        <Link to = '/about'>
-                                            About
-                                        </Link>
-                                    </li>
-                                    <li className = 'navbar-item'>
-                                        <div className="dropdown">
-                                            <button className="dropbtn">User</button>
-                                            <div className="dropdown-content">
-                                                <a href="/login">Login</a>
-                                                <a href="/signup">Signup</a>
-                                                <a href = '/user'>Dashboard</a>
+                            <div className = 'navbar-list'>
+                                <div className = {`navbar-items ${navClass}`}>
+                                    <ul>
+                                        <li className = 'navbar-item' onClick = {this.toggleSwitch}>
+                                            <Link to = '/'>
+                                                Home
+                                            </Link>
+                                        </li>
+                                        <li className = 'navbar-item' onClick = {this.toggleSwitch}>
+                                            <Link to = '/menu'>
+                                                Menu
+                                            </Link>
+                                        </li>
+                                        <li className = 'navbar-item' onClick = {this.toggleSwitch}>
+                                            <Link to = '/contact'>
+                                                Contact
+                                            </Link>
+                                        </li>
+                                        <li className = 'navbar-item' onClick = {this.toggleSwitch}>
+                                            <Link to = '/about'>
+                                                About
+                                            </Link>
+                                        </li>
+                                        <li className = 'navbar-item'>
+                                            <div className="dropdown">
+                                                <button className="dropbtn">User</button>
+                                                <div className="dropdown-content">
+                                                    <a href="/login" onClick = {this.toggleSwitch}>Login</a>
+                                                    <a href="/signup" onClick = {this.toggleSwitch}>Signup</a>
+                                                    <a href = '/user' onClick = {this.toggleSwitch}>Dashboard</a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
+                            <div className = 'navbar-toggle' onClick = {this.toggleSwitch}></div>
                         </div>
-                        <div className = 'navbar-toggle'></div>
                     </div>
                 </div>
             </div>
-        </div>
-    )    
+        )
+    }
 }
 
 
