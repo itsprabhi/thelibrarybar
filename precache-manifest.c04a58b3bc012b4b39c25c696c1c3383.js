0d5ce7f9bed51e4814e41a5e48d0c4670b9a8771
itsprabhi <iamprabhie@gmail.com>
2020-02-15 21:14:43 -0500

    Updates

---
+++ precache-manifest.c04a58b3bc012b4b39c25c696c1c3383.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2f35f8d690be9b646d938f04138095",
+    "url": "/thelibrarybar/index.html"
+  },
+  {
+    "revision": "7b233c49d7d5f12fb329",
+    "url": "/thelibrarybar/static/css/main.2bb0c2f4.chunk.css"
+  },
+  {
+    "revision": "7341b508d17612152c00",
+    "url": "/thelibrarybar/static/js/2.1a57fb9f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/thelibrarybar/static/js/2.1a57fb9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b233c49d7d5f12fb329",
+    "url": "/thelibrarybar/static/js/main.871ce78d.chunk.js"
+  },
+  {
+    "revision": "081fc4ecc3acf1da9c4a",
+    "url": "/thelibrarybar/static/js/runtime-main.a9f6a2f9.js"
+  },
+  {
+    "revision": "37f34dc0059d76d5bed0d87e8e7b7528",
+    "url": "/thelibrarybar/static/media/gallery-five.37f34dc0.jpg"
+  },
+  {
+    "revision": "998ab4ca6ef9cfb8e0b772e6fa82557f",
+    "url": "/thelibrarybar/static/media/gallery-four.998ab4ca.jpg"
+  },
+  {
+    "revision": "aa05c4c4da5a151c457d3ddabdcb3c80",
+    "url": "/thelibrarybar/static/media/gallery-one.aa05c4c4.jpg"
+  },
+  {
+    "revision": "fe233dd9362f8896121d38d8f9c72de9",
+    "url": "/thelibrarybar/static/media/gallery-three.fe233dd9.jpg"
+  },
+  {
+    "revision": "029a258a003b8684864d6c7eb5e14ba8",
+    "url": "/thelibrarybar/static/media/gallery-two.029a258a.jpg"
+  },
+  {
+    "revision": "b82ff8f35e84be4cfdcf77c6c12b8223",
+    "url": "/thelibrarybar/static/media/home-background.b82ff8f3.jpg"
+  },
+  {
+    "revision": "9effa5c10669fb5373ad854425eb86d7",
+    "url": "/thelibrarybar/static/media/home-food.9effa5c1.png"
+  }
+]);
